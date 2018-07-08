6ab59cb2f0b555a279a7dce118b5f71f8f1c607f
Ivan Gayton <ivangayton@gmail.com>
2018-07-08 14:50:13 +0300

    renamed downloader and made it create file-specific local folder for tiles

--- download_all_tiles_in_csv.py
+++ download_all_tiles_in_csv.py
@@ -1,5 +1,5 @@
 #!/bin/python
-"""Downloading necessary tiles for creation of an MBTile dataset.
+"""Download necessary tiles for creation of an MBTile dataset.
 
 Arguments:
     [1] A CSV file or URL created by create_csv.py containing tile URLs
@@ -10,7 +10,7 @@ Output:
     Pile 'o' tiles from a tileserver
 
 Example:
-    python3 multi_tile_downloader.py mylist.csv  
+    python3 download_all_tiles_in_csv.py mylist.csv  
 """
 import sys
 import os
@@ -19,19 +19,19 @@ import csv
 import time
 import urllib.request
 
-def managechunk(chunk):
+def managechunk(chunk, outdirpath):
     """Downloads all tiles contained in a chunk (list of tile rows)"""
     for item in chunk:
         tile_row = item[0].split(';')
         url = (tile_row[4])
-        outfilename = ('local_data/tiles/from_dg/{}_{}_{}.png'
-                       .format(tile_row[1], tile_row[2], tile_row[3]))
+        outfilename = ('{}{}_{}_{}.png'
+                       .format(outdirpath, tile_row[1], tile_row[2], tile_row[3]))
         rawdata = urllib.request.urlopen(url).read()
         print('Thread {} Writing {}'.format(threading.get_ident(),outfilename))
         with open(outfilename, 'wb') as outfile:
             outfile.write(rawdata)
 
-def task(inlist, num_threads):
+def task(inlist, num_threads, outdirpath):
     header_row = inlist.pop(0)
     # Break the list into chunks of approximately equal size
     chunks = [inlist[i::num_threads] for i in range(num_threads)]
@@ -39,7 +39,7 @@ def task(inlist, num_threads):
     threads = []
     for chunk in chunks:
 
-        thread = threading.Thread(target=managechunk, args=(chunk,))
+        thread = threading.Thread(target=managechunk, args=(chunk, outdirpath))
         threads.append(thread)
         thread.start()
     for thread in threads:
@@ -47,15 +47,17 @@ def task(inlist, num_threads):
 
 def main(infile):
     """Eat CSV of tile urls, spit out folder full of tiles"""
-    start = time.time()
-
     (infilename, extension) = os.path.splitext(infile)
+    outdirpath = '{}/'.format(infilename)
+    if not os.path.exists(outdirpath):
+        outdir = os.makedirs(outdirpath)
     threads_to_use=50
-
+    
+    start = time.time()
     with open(infile) as csvfile:
         reader = csv.reader(csvfile)
         tile_rows = list(reader)
-        task(tile_rows, threads_to_use)
+        task(tile_rows, threads_to_use, outdirpath)
 
     end = time.time() - start
     print('Finished. Downloading took {} seconds'.format(end))
