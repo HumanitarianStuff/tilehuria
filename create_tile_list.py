a7c4fb615cceb6d93fdd63190e77a25f5db3a70a
Ivan Gayton <ivangayton@gmail.com>
2018-09-08 11:35:46 +0300

    Hard-coded random switches as temporary workaround until fix JOSM-style URL strings

--- create_tile_list.py
+++ create_tile_list.py
@@ -1,5 +1,4 @@
 #!/bin/python3
-# Ivan Buendia Gayton, Humanitarian OpenStreetMap Team/Ramani Huria 2018
 """Create a CSV document containing a list of URLs of tiles from a Tile Map Service (TMS or tileserver).
 
 Arguments (using bash-style flags):
@@ -23,7 +22,7 @@ or, if the default zoom levels are not appropriate:
 python3 create_tile_list.py /path/to/myPolygon.geojson -minz 15 -maxz 21
 
 """
-#TODO: write up a proper docstring with the new argparse parameters and examples
+# Ivan Buendia Gayton, Humanitarian OpenStreetMap Team/Ramani Huria 2018
 
 import os
 import sys
@@ -31,23 +30,9 @@ import argparse
 from math import ceil
 import math
 from osgeo import ogr, osr
-
-def get_api_key(tileserver):
-    """Get an API key string from a local folder called api_keys"""
-    api_key = ''
-    try:
-        if tileserver == 'bing':
-            api_key = open('api_keys/bing_api_key.txt').read()
-        elif tileserver == 'digital_globe_standard':
-            api_key = open('api_keys/dg_standard_api_key.txt').read()
-        elif tileserver == 'google':
-            api_key = open('api_keys/google_api_key.txt').read()
-        else:
-            pass
-    except:
-        print("Something is wrong with your API key."
-               "Do you even have an API key?")
-    return api_key
+import csv
+import re
+import random
 
 def get_ogr_driver(extension):
     driver = None
@@ -79,21 +64,43 @@ def pixel_coords_to_tile_address(x,y):
     y = int(math.floor(y / 256))
     return (x, y)
 
-def tile_coords_zoom_and_tileserver_to_URL(TileX, TileY, zoom, tileserver, api_key):
+def fill_placeholders_in_url(url):
+    """Randomized replacement from a switch statement in curly braces within a URL"""
+    regex = r"\{(.*?)\}"
+    matches = re.finditer(regex, urlf, re.MULTILINE | re.DOTALL)
+    for matchNum, match in enumerate(matches):
+        for groupNum in range(0, len(match.groups())):
+            print(match.group(1))
+
+def tile_coords_zoom_and_tileserver_to_URL(TileX, TileY, zoom, tileserver):
     """Create a URL for a tile based on tile coordinates and zoom"""
     URL = ''
     if tileserver=='bing':
         quadKey = tile_coords_and_zoom_to_quadKey(
             int(TileX),int(TileY),int(zoom))
-        URL = quadKey_to_Bing_URL(quadKey, api_key)
+        switchserver = random.choice([0,1,2,3])
+        URL = ("http://t0.tiles.virtualearth.net/tiles/a{}.jpeg?"
+                "g=854&mkt=en-US&token="
+               "AopsdXjtTu-IwNoCTiZBtgRJ1g7yPkzAi65nXplc-eLJwZHYlAIf2yuSY_Kjg3Wn"
+               .format(switchserver, quadKey))
     elif tileserver=='digital_globe_standard':
-        switchserver='a' #TODO: make this alternate between a-d
+        switchserver = random.choice(['a', 'b', 'c', 'd'])
         URL = ("https://{}.tiles.mapbox.com/v4/digitalglobe.0a8e44ba"
-               "/{}/{}/{}.png?access_token={}"
-               .format(switchserver, zoom, TileX, TileY, api_key))
+               "/{}/{}/{}.png?access_token="
+               "pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYSI6ImNqZGFrZ3pjczNpaH"
+               "YycXFyMGo0djY3N2IifQ.90uebT4-ow1uqZKTUrf6RQ"
+               .format(switchserver, zoom, TileX, TileY))
+    elif tileserver=='digital_globe_premium':
+        switchserver = random.choice(['a', 'b', 'c', 'd'])
+        URL = ("https://{}.tiles.mapbox.com/v4/digitalglobe.316c9a2e"
+               "/{}/{}/{}.png?access_token="
+               "pk.eyJ1IjoiZGlnaXRhbGdsb2JlIiwiYSI6ImNqZGFrZ2c2dzFlMW"
+               "gyd2x0ZHdmMDB6NzYifQ.9Pl3XOO82ArX94fHV289Pg"
+               .format(switchserver, zoom, TileX, TileY))
     elif tileserver=='google':
-        URL = ("https://mt0.google.com/vt/lyrs=s&hl=en&x={}&y={}&z={}\n"
-               .format(TileX, TileY, zoom))
+        switchserver = random.choice([0,1,2,3]) 
+        URL = ("https://mt{}.google.com/vt/lyrs=s&hl=en&x={}&y={}&z={}"
+               .format(switchserver, TileX, TileY, zoom))
     elif tileserver=='osm':
         pass
     elif tileserver=='custom':
@@ -103,7 +110,7 @@ def tile_coords_zoom_and_tileserver_to_URL(TileX, TileY, zoom, tileserver, api_k
     return URL
 
 def tile_coords_and_zoom_to_quadKey(x, y, zoom):
-    """Create a quadkey for use with certain tileservers that use them."""
+    """Create a quadkey from xyzoom coordinates for Bing-style tileservers."""
     quadKey = ''
     for i in range(zoom, 0, -1):
         digit = 0
@@ -115,25 +122,18 @@ def tile_coords_and_zoom_to_quadKey(x, y, zoom):
         quadKey += str(digit)
     return quadKey
 
-def quadKey_to_Bing_URL(quadKey, api_key):
-    """Create a URL linking to a Bing tile server"""
-    tile_url = ("http://t0.tiles.virtualearth.net/tiles/a{}.jpeg?"
-                "g=854&mkt=en-US&token={}".format(quadKey, api_key))
-    return tile_url
-
 def main(infile, minzoom, maxzoom, tileserver, laytype, version, desc):
     """Read a polygon file and create a set of output files to create tiles"""
     (infilename, extension) = os.path.splitext(infile)
-    api_key = get_api_key(tileserver)
     driver = get_ogr_driver(extension)
     datasource = driver.Open(infile, 0)
     layer = datasource.GetLayer()
     layer_defn = layer.GetLayerDefn()
     extent = layer.GetExtent()
     (xmin, xmax, ymin, ymax) = (extent[0], extent[1], extent[2], extent[3])
-    geomcol = ogr.Geometry(ogr.wkbGeometryCollection)
+    geomcollection = ogr.Geometry(ogr.wkbGeometryCollection)
     for feature in layer:
-        geomcol.AddGeometry(feature.GetGeometryRef())
+        geomcollection.AddGeometry(feature.GetGeometryRef())
 
     # Create the main output file which will contain the URL list
     #TODO use the CSV library for this instead of just writing strings
@@ -141,7 +141,9 @@ def main(infile, minzoom, maxzoom, tileserver, laytype, version, desc):
     if os.path.exists(outfile):
         os.remove(outfile)
     output_csv = open(outfile, 'w')
-    output_csv.write('wkt;TileX;TileY;TileZ;URL\n')
+    writer = csv.writer(output_csv, delimiter = ';')
+    writer.writerow(['wkt','Tilex','TileY','TileZ','URL'])
+#    output_csv.write('wkt;TileX;TileY;TileZ;URL\n')
 
     # Create a new geographical file of the same type as the input file
     # which will contain polygon outlines of all tiles
@@ -205,18 +207,14 @@ def main(infile, minzoom, maxzoom, tileserver, laytype, version, desc):
                 poly.AddGeometry(ring)
     
                 # Check if tile is within the polygon of the Area of Interest.
-                intersect = geomcol.Intersect(poly)
+                intersect = geomcollection.Intersect(poly)
                 if intersect == True:
                     # Tile is in the AOI. Add a row to the CSV for this tile.
-                    outline = poly.ExportToWkt()
+                    wkt_outline = '\"{}\"'.format(poly.ExportToWkt())
                     URL = tile_coords_zoom_and_tileserver_to_URL(
-                        int(TileX), int(TileY), int(zoom),
-                        tileserver, api_key)
-                    #TODO: use CSV library instead of this brittle mess of a string
-                    output_csv.write('\"'+outline+'\";'+str(TileX)+';'
-                                         +str(TileY)+';'+str(zoom)+';'
-                                         + URL)
-    
+                        int(TileX), int(TileY), int(zoom), tileserver)    
+                    writer.writerow([wkt_outline,
+                                     str(TileX), str(TileY), str(zoom), URL])
                     outFeature = ogr.Feature(featureDefn)
                     outFeature.SetGeometry(poly)
                     if extension == 'kml':
@@ -279,6 +277,7 @@ if __name__ == "__main__":
     laytype = args.type if args.type else 'overlay'
     version = args.version if args.version else '1.1'
     desc = args.description if args.description else 'A set of MBTiles'
-    
+
+    print('tileserver is {}'.format(tileserver))
 
     main(infile, minz, maxz, tileserver, laytype, version, desc)
